d8f33bee9f76340da740657bbf379494e7dde30d
33034三関蛍 <33034@ojs.ac.jp>
2022-05-10 15:54:24 +0900

    練習問題2.1.4完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,23 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+            //2.1.3
+            var songs = new Song[] {
+                new Song("Let it be", "The Beatles", 234),
+                new Song("Let it be", "The Beatles", 243),
+                new Song("Let it be", "The Beatles", 252),
+                new Song("Let it be", "The Beatles", 261),
+                new Song("Let it be", "The Beatles", 270),
+            };
+
+            PrintSong(songs);
+        }
+
+        //2.1.4
+        private static void PrintSong(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine("{0}, {1}, {2:m\\:ss}",song.Title, song.ArtistName, TimeSpan.FromSeconds(song.Length));
+            }
         }
     }
 }
