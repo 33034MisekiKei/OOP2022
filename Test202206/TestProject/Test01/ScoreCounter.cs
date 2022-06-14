5f177aa2591ff1e8e42383011acd411d5bc10633
33034三関蛍 <33034@ojs.ac.jp>
2022-06-14 15:53:52 +0900

    統一試験　完成

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -8,32 +8,35 @@ namespace Test01 {
         // コンストラクタ
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
-
-
-
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
-
-
-
-
-
-
-            
+            List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student sale = new Student {
+                    Name = items[0],
+                    Subiect = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
-
-
-
-
-
-            
+            var dict = new Dictionary<string, int>();
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Name))
+                    dict[sale.Subiect] += sale.Score;
+                else
+                    dict[sale.Subiect] = sale.Score;
+            }
+            return dict;
         }
     }
 }
