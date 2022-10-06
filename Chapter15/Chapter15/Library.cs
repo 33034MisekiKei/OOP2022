588226978634cd8d75c0d5db401332a7aebaf36f
33034三関蛍 <33034@ojs.ac.jp>
2022-10-06 11:36:57 +0900

    P379

--- Chapter15/Chapter15/Library.cs
+++ Chapter15/Chapter15/Library.cs
@@ -18,11 +18,11 @@ namespace Chapter15 {
 
         public string Title { get; set; }
         public int Price { get; set; }
-        public int CategoryId { get; set; }
+        public int Category { get; set; }
         public int PublishedYear { get; set; }
 
         public override string ToString() {
-            return $"発行年:{PublishedYear}, カテゴリ:{CategoryId}, 価格:{Price}, タイトル:{Title}";
+            return $"発行年:{PublishedYear}, カテゴリ:{Category}, 価格:{Price}, タイトル:{Title}";
         }
     }
 
@@ -43,25 +43,25 @@ namespace Chapter15 {
             };
 
             Books = new List<Book> {
-                new Book { Title = "Writing C# Solid Code", CategoryId = 1, Price = 2500, PublishedYear = 2016 },
-                new Book { Title = "C#開発指南", CategoryId = 1, Price = 3800, PublishedYear = 2014 },
-                new Book { Title = "Visual C#再入門", CategoryId = 1, Price = 2780, PublishedYear = 2016 },
-                new Book { Title = "フレーズで学ぶC# Book", CategoryId = 1, Price = 2400, PublishedYear = 2016 },
-                new Book { Title = "TypeScript初級講座", CategoryId = 1, Price = 2500, PublishedYear = 2015 },
-                new Book { Title = "PowerShell 実践レシピ", CategoryId = 2, Price = 4200, PublishedYear = 2013 },
-                new Book { Title = "SQL Server 完全入門", CategoryId = 2, Price = 3800, PublishedYear = 2014 },
-                new Book { Title = "IIS Webサーバー運用ガイド", CategoryId = 2, Price = 3180, PublishedYear = 2015 },
-                new Book { Title = "Microsoft Azureサーバー構築", CategoryId = 2, Price = 4800, PublishedYear = 2016 },
-                new Book { Title = "Webデザイン講座 HTML5＆CSS", CategoryId = 3, Price = 2800, PublishedYear = 2013 },
-                new Book { Title = "HTML5 Web大百科", CategoryId = 3, Price = 3800, PublishedYear = 2015 },
-                new Book { Title = "CSSデザイン 逆引き辞典", CategoryId = 3, Price = 3550, PublishedYear = 2015 },
-                new Book { Title = "Windows10で楽しくお仕事", CategoryId = 4, Price = 2280, PublishedYear = 2016 },
-                new Book { Title = "Windows10使いこなし術", CategoryId = 4, Price = 1890, PublishedYear = 2015 },
-                new Book { Title = "続Windows10使いこなし術", CategoryId = 4, Price = 2080, PublishedYear = 2016 },
-                new Book { Title = "Windows10 やさしい操作入門", CategoryId = 4, Price = 2300, PublishedYear = 2015 },
-                new Book { Title = "まるわかりMicrosoft Office入門", CategoryId = 5, Price = 1890, PublishedYear = 2015 },
-                new Book { Title = "Word・Excel実践テンプレート集", CategoryId = 5, Price = 2600, PublishedYear = 2016 },
-                new Book { Title = "たのしく学ぶExcel初級編", CategoryId = 5, Price = 2800, PublishedYear = 2015 },
+                new Book { Title = "Writing C# Solid Code", Category = 1, Price = 2500, PublishedYear = 2016 },
+                new Book { Title = "C#開発指南", Category = 1, Price = 3800, PublishedYear = 2014 },
+                new Book { Title = "Visual C#再入門", Category = 1, Price = 2780, PublishedYear = 2016 },
+                new Book { Title = "フレーズで学ぶC# Book", Category = 1, Price = 2400, PublishedYear = 2016 },
+                new Book { Title = "TypeScript初級講座", Category = 1, Price = 2500, PublishedYear = 2015 },
+                new Book { Title = "PowerShell 実践レシピ", Category = 2, Price = 4200, PublishedYear = 2013 },
+                new Book { Title = "SQL Server 完全入門", Category = 2, Price = 3800, PublishedYear = 2014 },
+                new Book { Title = "IIS Webサーバー運用ガイド", Category = 2, Price = 3180, PublishedYear = 2015 },
+                new Book { Title = "Microsoft Azureサーバー構築", Category = 2, Price = 4800, PublishedYear = 2016 },
+                new Book { Title = "Webデザイン講座 HTML5＆CSS", Category = 3, Price = 2800, PublishedYear = 2013 },
+                new Book { Title = "HTML5 Web大百科", Category = 3, Price = 3800, PublishedYear = 2015 },
+                new Book { Title = "CSSデザイン 逆引き辞典", Category = 3, Price = 3550, PublishedYear = 2015 },
+                new Book { Title = "Windows10で楽しくお仕事", Category = 4, Price = 2280, PublishedYear = 2016 },
+                new Book { Title = "Windows10使いこなし術", Category = 4, Price = 1890, PublishedYear = 2015 },
+                new Book { Title = "続Windows10使いこなし術", Category = 4, Price = 2080, PublishedYear = 2016 },
+                new Book { Title = "Windows10 やさしい操作入門", Category = 4, Price = 2300, PublishedYear = 2015 },
+                new Book { Title = "まるわかりMicrosoft Office入門", Category = 5, Price = 1890, PublishedYear = 2015 },
+                new Book { Title = "Word・Excel実践テンプレート集", Category = 5, Price = 2600, PublishedYear = 2016 },
+                new Book { Title = "たのしく学ぶExcel初級編", Category = 5, Price = 2800, PublishedYear = 2015 },
             };
         }
     }
--- Chapter15/Chapter15/Program.cs
+++ Chapter15/Chapter15/Program.cs
@@ -8,13 +8,64 @@ namespace Chapter15 {
     class Program {
         static void Main(string[] args) {
 
-            var books = Library.Books
-                .OrderBy(b => b.CategoryId)
-                .ThenByDescending(b => b.PublishedYear);
+            var years = new List<int>();
+            int year;
 
-            foreach(var book in books) 
+            Console.WriteLine("出力したい西暦を入力（終了：-1）");
+            year = int.Parse(Console.ReadLine());
+            while (year != -1) {
+                years.Add(year);
+                year = int.Parse(Console.ReadLine());
+            }
+
+            int sort;
+
+            Console.WriteLine();
+            Console.Write("昇順：1 降順：2 ：");
+            sort = int.Parse(Console.ReadLine());
+
+            IEnumerable<Book> books;
+            if (sort == 1) 
+            { //昇順
+                books = Library.Books
+                    .Where(b => years.Contains(b.PublishedYear))
+                    .OrderBy(b => b.PublishedYear);
+            }
+            else 
+            { //降順
+                books = Library.Books
+                    .Where(b => years.Contains(b.PublishedYear))
+                    .OrderByDescending(b => b.PublishedYear);
+            }
+
+            //var years = new int[] { 2013, 2016 };
+            foreach (var book in books) 
+           　{
+                Console.WriteLine(book);
+            }
+
+            Console.WriteLine();
+            //var groups = Library.Books
+            //    .Where(b => years.Contains(b.PublishedYear))
+            //    .GroupBy(b => b.PublishedYear)
+            //    .OrderBy(g => g.Key);
+
+            var selected = Library.Books
+                .OrderBy(b => b.Category)
+                .ThenBy(b => b.PublishedYear)
+                .Join(Library.Categories        //結合する2番目のシーケンス
+                    ,book => book.Category      //対象シーケンスの結合キー
+                    ,category => category.Id    //2番目のシーケンスの結合キー
+                    ,(book, category) => new {
+                        Title = book.Title
+                        ,Category = category.Name
+                        ,PublishedYear = book.PublishedYear});
+
+            foreach (var book in books) 
             {
-                Console.WriteLine(books);
+                //Console.WriteLine($"{book.PublishedYear}年");
+                //var category = Library.Categories.Where(b => b.Id == book.CategoryId).First();
+                Console.WriteLine($"{book.Title} {book.Category} {book.PublishedYear}");
             }
         }
     }
