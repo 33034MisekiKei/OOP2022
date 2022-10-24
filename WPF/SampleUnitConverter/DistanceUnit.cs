cf552fb14f6967c6c10866e1e4d3e5390c1068f9
33034三関蛍 <33034@ojs.ac.jp>
2022-10-24 15:57:47 +0900

    CollarChecker まだできていない

--- WPF/SampleUnitConverter/DistanceUnit.cs
+++ WPF/SampleUnitConverter/DistanceUnit.cs
@@ -20,7 +20,7 @@ namespace SampleUnitConverter {
             new MetricUnit{Name = "mm", Coefficient = 1,},
             new MetricUnit{Name = "cm", Coefficient = 10,},
             new MetricUnit{Name = "m", Coefficient = 10 * 100,},
-            new MetricUnit{Name = "km", Coefficient = 10 * 100 * 100,},
+            new MetricUnit{Name = "km", Coefficient = 10 * 100 * 1000,},
         };
 
         public static ICollection<MetricUnit> Units { get { return units; } }
