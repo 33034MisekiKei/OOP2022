98e6e3c1d8a84d4ae8956620ceb3fade4d9471f5
33034三関蛍 <33034@ojs.ac.jp>
2022-10-27 11:06:12 +0900

    CollarChecker

--- WPF/CollarChecker/MainWindow.xaml.cs
+++ WPF/CollarChecker/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -14,12 +15,34 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace CollarChecker {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
     public partial class MainWindow : Window {
+        //コンストラクタ
         public MainWindow() {
             InitializeComponent();
+
+            DataContext = GetColorList(); //←追加
+
         }
+
+        /// <summary>
+        /// すべての色を取得するメソッド
+        /// </summary>
+        /// <returns></returns>
+        private MyColor[] GetColorList() {
+            return typeof(Colors).GetProperties(BindingFlags.Public | BindingFlags.Static)
+                .Select(i => new MyColor() { Color = (Color)i.GetValue(null), Name = i.Name }).ToArray();
+        }
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+
+        }
+
+    }
+
+    /// <summary>
+    /// 色と色名を保持するクラス
+    /// </summary>
+    public class MyColor {
+        public Color Color { get; set; }
+        public string Name { get; set; }
     }
 }
